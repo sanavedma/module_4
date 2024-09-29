5965e6bcd0c332be6cc1f61b165202230d9f80dd
Oksana Efremova <124363708+sanavedma@users.noreply.github.com>
2024-09-29 18:53:05 +0500

    Add files via upload
    to consolidate the skill of creating and importing modules, as well as functions and variables contained in them.

---
+++ module_4_1.py
@@ -0,0 +1,7 @@
+from true_math import divide as true_divide
+from fake_math import divide as fake_divide
+
+print(fake_divide(69, 3))
+print(fake_divide(3, 0))
+print(true_divide(49, 7))
+print(true_divide(15, 0))
