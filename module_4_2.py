fe36617a48f1bb79287eb783607ef8cacaf40a56
Oksana Efremova <124363708+sanavedma@users.noreply.github.com>
2024-10-13 16:36:07 +0500

    module_4_2.py

--- module_4_2.py
+++ module_4_2.py
@@ -3,4 +3,5 @@ def test_function():
         print('Я в области видимости функции test_function')
     inner_function()
 
-inner_function()
+# inner_function() функция не работает, потому что она находится в локальной области видимости, а не глобальной
+test_function()
